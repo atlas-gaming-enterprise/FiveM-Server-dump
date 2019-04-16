2dbd09663893e0ffb0a326e1081e874a21174d1e
radcrazykid2 <49612178+radcrazykid2@users.noreply.github.com>
2019-04-16 05:30:15 -0400

    Delete update_sqlite_27(1).sql

--- FiveM Server/ATLAS-teamspeak3-server/sql/update_sqlite_27(1).sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE revocations (
-  revocation_key VARCHAR(44) NOT NULL,
-  revocation_type INTEGER UNSIGNED NOT NULL,
-  revocation_expiration INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY(revocation_type, revocation_key)
-);
