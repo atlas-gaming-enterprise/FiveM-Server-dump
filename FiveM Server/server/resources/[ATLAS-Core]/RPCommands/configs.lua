7fb46429fd35d8c80285d0398a85b4c5000c57de
radcrazykid2 <49612178+radcrazykid2@users.noreply.github.com>
2019-04-16 04:13:55 -0400

    Add files via upload

---
+++ FiveM Server/server/resources/[ATLAS-Core]/RPCommands/configs.lua
@@ -0,0 +1,7 @@
+--CONFIGS:
+
+enable_core_commands = true --[//  /me  /it]--
+enable_roleplay_commands = true --[/r  /dispatch  /911  /rpname /roll]--
+enable_police_commands = true --[/p  /20  /id  /32  /8  /8f  /7  /7f  /7g  /7m  /7i  /6  /47  /23  /code4  /80  /4  /11  /28  /29]--
+
+btime = 20 --Amount of time the blip stays on map when Panic Button is pressed (In settings)
