cd98999bcdc5ad1cc5d5748067a6d91d2b1885f8
radcrazykid2 <49612178+radcrazykid2@users.noreply.github.com>
2019-04-16 23:41:16 -0400

    Update

---
+++ FiveM Server/server/resources/[test]/keks/__resource.lua
@@ -0,0 +1,12 @@
+files {
+    'index.html',
+    'loadscreen.css',
+	--'keks,css',
+	'bankgothic.ttf',
+    'loadscreen.jpg',
+    --'music/Loading.ogg',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
