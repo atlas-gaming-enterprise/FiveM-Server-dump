649fd6caa2dbf5224f4c4ed3071df07aaf4ff647
radcrazykid2 <49612178+radcrazykid2@users.noreply.github.com>
2019-04-17 06:53:10 -0400

    Update

---
+++ FiveM Server/server/resources/[Police]/xx-car-cvpi/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+data_file 'VEHICLE_METADATA_FILE' 'meta/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/carvariations.meta'
+
+files {
+  'meta/vehicles.meta',
+  'meta/carcols.meta',
+  'meta/carvariations.meta'
+}
+
+--client_script 'vehicle_names.lua'
